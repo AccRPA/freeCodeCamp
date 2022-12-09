b5559d7e89d8d9b9ee0722d27aa8d32833c8c377
AccRPA <accrpaw10@gmail.com>
2022-12-09 18:47:38 +0100

    Update smallest_common_multiple.js
    Second version that causes infinite loop in line 12

--- smallest_common_multiple.js
+++ smallest_common_multiple.js
@@ -1,4 +1,7 @@
-function smallestCommonMultiple(min, max, from){
+function smallestCommons(arr){
+  const newArr = arr.sort((a, b) => a - b);
+  let min = newArr[0];
+  let max = newArr[1];
   let found = false;
   let index = 1;
 
@@ -9,30 +12,32 @@ function smallestCommonMultiple(min, max, from){
   while (!found) {
     let minMul = min * index;
     let maxMul = max * index;
-    if (arrMax.indexOf(minMul) != -1 && minMul > from) {
+    if (arrMax.indexOf(minMul) !== -1) {
       found = true;
       result = minMul;
-    } else if (arrMin.indexOf(maxMul) != -1 && maxMul > from) {
+    } else if (arrMin.indexOf(maxMul) !== -1) {
       found = true;
       result = maxMul;
     }
     arrMin.push(minMul);
     arrMax.push(maxMul);
+
+    if (found){
+      for (let i = min; i <= max; i++){
+          found &= (result % i === 0);
+      }
+    }
     index++;
   }
   return result;
 }
 
-function smallestCommons(arr) {
-  const newArr = arr.sort((a, b) => a - b);
-  let min = newArr[0];
-  let max = newArr[1];
-  let result = smallestCommonMultiple(min, max, 0);
-  console.log(result);
-  return result;
-  
-}
-
 console.log(
-  smallestCommons([5, 1])
-)
+  //smallestCommons([1, 3])
+  //smallestCommons([1, 5]) //should return a number.
+  //smallestCommons([1, 5]) //should return 60.
+  //smallestCommons([5, 1]) //should return 60.
+  smallestCommons([2, 10]) //should return 2520.
+  //smallestCommons([1, 13]) //should return 360360.
+  //smallestCommons([23, 18]) //should return 6056820
+);
