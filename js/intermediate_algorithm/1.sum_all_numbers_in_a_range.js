a3bdb6060f5d06128013c5b58f5e1af2bc429a6e
AccRPA <accrpaw10@gmail.com>
2022-12-10 18:41:58 +0100

    Update 1.sum_all_numbers_in_a_range.js
    Solution accepted

--- js/intermediate_algorithm/1.sum_all_numbers_in_a_range.js
+++ js/intermediate_algorithm/1.sum_all_numbers_in_a_range.js
@@ -1,11 +1,18 @@
 function sumAll(arr) {
-  return 1;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+  
+  for (let i = min; i <= max; i++){
+    sum += i;
+  }
+  return sum;
 }
 
 console.log(
-  sumAll([1, 4]) //should return a number.
+  //sumAll([1, 4]) //should return a number.
   //sumAll([1, 4]) //should return 10.
   //sumAll([4, 1]) //should return 10.
   //sumAll([5, 10]) //should return 45.
-  //sumAll([10, 5]) //should return 45.
+  sumAll([10, 5]) //should return 45.
 );
