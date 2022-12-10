abbe3668385203ed3e2b43697801e739feff878c
AccRPA <accrpaw10@gmail.com>
2022-12-10 18:32:30 +0100

    Create 1.sum_all_numbers_in_a_range.js
    We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first. For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.

---
+++ js/intermediate_algorithm/1.sum_all_numbers_in_a_range.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+  return 1;
+}
+
+console.log(
+  sumAll([1, 4]) //should return a number.
+  //sumAll([1, 4]) //should return 10.
+  //sumAll([4, 1]) //should return 10.
+  //sumAll([5, 10]) //should return 45.
+  //sumAll([10, 5]) //should return 45.
+);
